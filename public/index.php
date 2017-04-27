49e43b54429ac351b223bfe4ee9a9397ee0d3360
sdshaoda <sdshaoda@126.com>
2017-04-27 19:20:27 +0800

    change twig-view to twig

--- public/index.php
+++ public/index.php
@@ -7,22 +7,30 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-//$loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
-//$twig = new Twig_Environment($loader, array(
-//    'cache' => __DIR__ . 'compilation_cache',
-//));
 
 $container = $app->getContainer();
 
 $container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/../templates', [
-//        'cache' => __DIR__ . '/../compilation_cache'
-    ]);
 
-    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+//    twig-view
 
-    return $view;
+//    $view = new \Slim\Views\Twig(__DIR__ . '/../templates', [
+////        'cache' => __DIR__ . '/../compilation_cache'
+//    ]);
+//
+//    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+//    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+//
+//    return $view;
+
+
+    $loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+    $twig = new Twig_Environment($loader, array(
+        //        'cache' => __DIR__ . 'compilation_cache',
+    ));
+
+    return $twig;
+
 };
 
 
@@ -33,9 +41,15 @@ require __DIR__ . '/../src/middleware.php';
 require __DIR__ . '/../src/routes.php';
 
 $app->get('/hello/{name}', function ($request, $response, $args) {
-    return $this->view->render($response, 'index.html', [
-        'name' => $args['name']
-    ]);
+
+
+    return $this->view->render('index.html', array('name' => $args['name']));
+
+//    twig-view
+
+//    return $this->view->render($response, 'index.html', [
+//        'name' => $args['name']
+//    ]);
 })->setName('index');
 
 $app->run();
